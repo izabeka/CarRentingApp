ab16bbc0600954b1f8c5bbdd7e6f4b792b03da4c
Fotsik <fotskontakt@gmail.com>
2019-12-05 20:08:53 +0100

    model uzytkownika

--- src/back-end/main.js
+++ src/back-end/main.js
@@ -1,6 +1,11 @@
-//Hello World na ścieżce / zapytania dla GET
+//Importy
 const express = require('express');
 const app = express();
+
+//Importy z innych plików
+
+
+//Hello World na ścieżce / zapytania dla GET
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -15,10 +20,11 @@ app.listen(port, () => {
 // Połączenie z bazą danych
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test?w=majority";
+const uri = "mongodb+srv://dostepdobazy:chomikiwitka@chomikiwitka-a2ubx.gcp.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
- // perform actions on the collection object
+  // perform actions on the collection object
   client.close();
 });
+
