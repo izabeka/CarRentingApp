ddd94986a5c6d6ba4a39ddc2e67095aacd3ada43
Fotsik <fotskontakt@gmail.com>
2019-12-05 18:55:53 +0100

    polaczenie z baza danych

--- src/back-end/main.js
+++ src/back-end/main.js
@@ -11,3 +11,14 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Nasluchuje portu ${port}...`);
 })
+
+// Połączenie z bazą danych
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/test?w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+ // perform actions on the collection object
+  client.close();
+});
