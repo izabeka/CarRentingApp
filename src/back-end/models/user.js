6888d8037e3ca727a9b6485db5f5f3bfb450ddde
izabeka <57036285+izabeka@users.noreply.github.com>
2019-12-14 13:35:15 +0100

    login and tokens added (#5)

--- src/back-end/models/user.js
+++ src/back-end/models/user.js
@@ -31,7 +31,6 @@ const userSchema = new mongoose.Schema({
     },
     confirmPassword: {
         type: String,
-        required: true,
         minlength: 8,
         maxlength: 1024,
     },
@@ -45,7 +44,7 @@ function validateUser(user){
     const schema = {
         login: Joi.string().min(3).max(50).required(),
         name: Joi.string().min(3).max(50).required(),
-        email: Joi.string().min(5).max(100).required().unique('user.emailAdress').email(),
+        email: Joi.string().min(5).max(100).required().email(),
         password: Joi.string().min(8).max(255).required(),
         confirmPassword: Joi.ref('password')
     }
@@ -53,7 +52,17 @@ function validateUser(user){
     return Joi.validate(user, schema);
 }
 
+function validateLogin(user) {
+    const schema = {
+        login: Joi.string().min(3).max(50).required(),
+        password: Joi.string().min(8).max(255).required()
+    };
+    
+    return Joi.validate(user, schema);
+};
+
 //exportowanie
 
 exports.User = User;
 exports.validateUser = validateUser;
+exports.validateLogin = validateLogin;
